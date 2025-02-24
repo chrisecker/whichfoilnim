3a22d540a9e9db521c8faaa7e45c03c6d23b0a23
C. Ecker <chrisecker@zoho.com>
2025-02-24 20:10:13 +0100

    Auswahl per Combobox testweise implementiert.

--- testcanvas.nim
+++ testcanvas.nim
@@ -1,14 +1,22 @@
 import canvas
 import foilmodel
+import airfoil
 import nigui
 import pixie
-
+import std/os
+import algorithm 
 
 when isMainModule:
 
   app.init()
 
   var window = newWindow()
+
+  var p = newLayoutContainer(Layout_Vertical)
+  window.add(p)
+  p.widthMode = WidthMode_Fill
+  p.heightMode = HeightMode_Expand
+
   var c = newCanvasCtrl()
   c.trafo = mat3(
       0.5, 0.0, 0.0,
@@ -16,12 +24,29 @@ when isMainModule:
       0.0, 0.0, 1.0
     )
 
+  p.add(c)
 
   c.widthMode = WidthMode_Fill
   c.heightMode = HeightMode_Expand
-  window.add(c)
 
+  # gtk_combo_box_text_new_with_entry
+  var b = newComboBox()
+  var options: seq[string] = @[]
 
+  var i = -1
+  for d in walkDir("foils", relative=false):
+    i += 1
+    if d.kind == pcFile:
+      options.add(d.path)
+  options.sort(system.cmp)
+  
+  b.options = options
+  p.add(b)
+
+  var button = newButton("Schieber einstellen")
+  p.add(button)
+
+  
   window.onKeyDown = proc(event: KeyboardEvent) =
     if event.key == Key_Plus:
       c.trafo = c.trafo*scale(vec2(1.1, 1.1))
@@ -42,15 +67,29 @@ when isMainModule:
     c.forceRedraw
 
     
-  var foil = newFoilFigure(open("devel/fx61168-il.dat"))
+  var foil = newFoilFigure("devel/fx61168-il.dat")
 
   foil.pa = vec2(100, 400)
   foil.pb = vec2(500, 400)
   foil.alpha = 90
   foil.l = 100
-  foil.su0 = 0.1
+  #foil.su0 = 0.1
   #foil2.pt = vec2(20, 0)
   c.figures.add(foil)
+
+  button.onClick = proc(event: ClickEvent) =
+    let (upper, lower) = foil.match_sliders(foil.airfoil)
+    foil.upper_values = upper
+    foil.lower_values = lower
+    c.forceRedraw    
+    
+  proc on_combo(event: ComboBoxChangeEvent) =
+    var path = ComboBox(event.control).value
+    echo "event: ", path 
+    foil.airfoil = load_airfoil(path)
+    c.forceRedraw
+  b.onChange = on_combo
+
 
 
   echo "Benutze +, -, left, right, up, down"
