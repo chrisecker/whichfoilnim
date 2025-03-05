845f7d572b4f9cdcd510e9919ef81c0506b3e23f
C. Ecker <chrisecker@zoho.com>
2025-03-05 16:50:48 +0100

    Buttons for matching and setting sliders.

--- testcanvas.nim
+++ testcanvas.nim
@@ -4,9 +4,12 @@ import airfoil
 import nigui
 import pixie
 import std/os
+import std/strutils
+# import std/unicode
 import algorithm 
 
 when isMainModule:
+  import std/cmdline
 
   app.init()
 
@@ -26,8 +29,14 @@ when isMainModule:
 
   p.add(c)
 
+
   c.widthMode = WidthMode_Fill
   c.heightMode = HeightMode_Expand
+
+  
+  if paramCount() >= 1:
+    let fn = commandLineParams()[0]
+    c.bgimage = readImage(fn)
 
   # gtk_combo_box_text_new_with_entry
   var b = newComboBox()
@@ -43,9 +52,12 @@ when isMainModule:
   b.options = options
   p.add(b)
 
-  var button = newButton("Schieber einstellen")
-  p.add(button)
+  var b_set = newButton("Schieber einstellen")
+  p.add(b_set)
 
+  var b_search = newButton("Profil suchen")
+  p.add(b_search)
+  
   
   window.onKeyDown = proc(event: KeyboardEvent) =
     if event.key == Key_Plus:
@@ -77,15 +89,46 @@ when isMainModule:
   #foil2.pt = vec2(20, 0)
   c.figures.add(foil)
 
-  button.onClick = proc(event: ClickEvent) =
+  b_set.onClick = proc(event: ClickEvent) =
     let (upper, lower) = foil.match_sliders(foil.airfoil)
     foil.upper_values = upper
     foil.lower_values = lower
-    c.forceRedraw    
+    c.forceRedraw
+
+  b_search.onClick = proc(event: ClickEvent) =
+      var testfoil: Airfoil
+      var best: Airfoil
+      var best_penalty = -1.0
+      var best_name: string
+      var b: float
+      var i = -1
+      for d in walkDir("foils", relative=false):
+        i += 1
+        if d.kind == pcFile and d.path.toLowerAscii.endsWith(".dat"):
+          #toLower.endswith(".dat"):
+          try:
+            testfoil = load_airfoil($d.path)
+          except:
+            echo "Kann nicht geladen werden", i, " ", d.path
+            continue
+
+          try:
+            b = foil.badness(testfoil)
+          except:
+            echo "Kann nicht berechnet werden", i, " ", d.path
+            continue
+            
+          if best_penalty<0 or b<best_penalty:
+            best_penalty = b
+            best_name = $d.path
+            best = testfoil
+      foil.airfoil = best
+      c.forceRedraw()
+      echo "Best: ", best_name, " ", best_penalty
+    
     
   proc on_combo(event: ComboBoxChangeEvent) =
     var path = ComboBox(event.control).value
-    echo "event: ", path 
     foil.airfoil = load_airfoil(path)
     c.forceRedraw
   b.onChange = on_combo
